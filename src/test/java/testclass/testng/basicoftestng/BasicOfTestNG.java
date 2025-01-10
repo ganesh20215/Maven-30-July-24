0f28b13fa44c9816358e6a0cd2aeb54893a7ea95
Ganesh Jadhav <36586236+ganesh20215@users.noreply.github.com>
2025-01-10 08:14:18 +0530

    Update BasicOfTestNG.java

--- src/test/java/testclass/testng/basicoftestng/BasicOfTestNG.java
+++ src/test/java/testclass/testng/basicoftestng/BasicOfTestNG.java
@@ -11,6 +11,7 @@ public class BasicOfTestNG {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.edso.in");
         driver.manage().window().maximize();
+        driver.close();
     }
 
     @Test
